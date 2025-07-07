43695c2a0eda0a6b2c58ec802a1ade48a6071a99
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-07-07 10:17:21 +0500

    Login And Register Routes Added

---
+++ src/app/auth/login/route.ts
@@ -0,0 +1,43 @@
+import dbConn from "@/lib/db";
+import { jsonResponse } from "@/lib/helpers/jsonResponse";
+import { User } from "@/lib/models/users";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from 'bcrypt'
+
+
+export const POST = async (req: NextRequest) => {
+    try {
+        await dbConn();
+        const body = await req.json()
+        const { email, password } = body;
+        if (!email || !password) {
+            return jsonResponse({
+                error: "Provide All Fields !"
+            }, 200)
+        }
+        let user = await User.findOne({ email: email });
+        if (!user) {
+            return jsonResponse({
+                error: "User Not Found!"
+            }, 200)
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(isPasswordValid) {
+            return jsonResponse({
+                success:true,
+                token:'',
+                user
+            },200)
+        }
+
+        return jsonResponse({
+            error:"Password is incorrect !"
+        }, 201);
+
+    } catch (error) {
+        console.log(error)
+        return jsonResponse({ error: "Internal Server Error" }, 200)
+    }
+
+}
